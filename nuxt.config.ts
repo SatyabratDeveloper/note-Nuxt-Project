ae013b058c1d0a9f9cb2980d64ceb731ed71e3f9
Satyabrat Jha <satyabrat.jha@vidyamantra.com>
2022-12-19 11:12:16 +0530

    Project started (Added Pinia store and tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: ["~/assets/css/tailwind.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    // modules: [
+    //     // ...
+    //     [
+    //       '@pinia/nuxt',
+    //       {
+    //         autoImports: [
+    //           // automatically imports `defineStore`
+    //           'defineStore', // import { defineStore } from 'pinia'
+    //           // automatically imports `defineStore` as `definePiniaStore`
+    //           ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+    //         ],
+    //       },
+    //     ],
+    // ],
+});
